44bbcf19308200ec0445a2bc9c42ff3138d4b5e2
assu_2000 <kayassurance@gmail.com>
2023-11-29 11:22:02 +0100

    grpc set up & use cases

--- src/main/java/org/assu/GrpcServer.java
+++ src/main/java/org/assu/GrpcServer.java
@@ -1,4 +1,18 @@
 package org.assu;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.assu.service.BankGrpcService;
+
+import java.io.IOException;
+
 public class GrpcServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(5555)
+                .addService(new BankGrpcService())
+                .build();
+
+        server.start();
+        server.awaitTermination();
+    }
 }
--- src/main/java/org/assu/service/BankGrpcService.java
+++ src/main/java/org/assu/service/BankGrpcService.java
@@ -4,6 +4,9 @@ import io.grpc.stub.StreamObserver;
 import org.assu.stubs.Bank;
 import org.assu.stubs.BankServiceGrpc;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
 
     @Override
@@ -25,17 +28,88 @@ public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
 
     @Override
     public void getCurrencyStream(Bank.ConvertCurrencyRequest request, StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
-        super.getCurrencyStream(request, responseObserver);
+        String currencyFrom = request.getCurrencyFrom();
+        String currencyTo = request.getCurrencyTo();
+        float amount = request.getAmount();
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            int counter = 0;
+            @Override
+            public void run() {
+                Bank.ConvertCurrencyResponse response = Bank.ConvertCurrencyResponse.newBuilder()
+                        .setCurrencyFrom(currencyFrom)
+                        .setCurrencyTo(currencyTo)
+                        .setResult((float) (amount * Math.random() * 10))
+                        .build();
+
+                responseObserver.onNext(response);
+                ++counter;
+                if (counter == 20) {
+                    responseObserver.onCompleted();
+                    timer.cancel();
+                }
+
+            }
+        }, 1000, 1000);
+
+
     }
 
     @Override
     public StreamObserver<Bank.ConvertCurrencyRequest> performStream(StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
-        return super.performStream(responseObserver);
+        return new StreamObserver<Bank.ConvertCurrencyRequest>() {
+            String currencyFrom;
+            String currencyTo;
+            float sum = 0;
+            @Override
+            public void onNext(Bank.ConvertCurrencyRequest convertCurrencyRequest) {
+                sum+= convertCurrencyRequest.getAmount();
+                currencyFrom = convertCurrencyRequest.getCurrencyFrom();
+                currencyTo = convertCurrencyRequest.getCurrencyTo();
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                Bank.ConvertCurrencyResponse response = Bank.ConvertCurrencyResponse.newBuilder()
+                        .setCurrencyFrom(currencyFrom)
+                        .setCurrencyTo(currencyTo)
+                        .setResult((float) (sum * Math.random() * 10))
+                        .build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+            }
+        };
     }
 
     @Override
     public StreamObserver<Bank.ConvertCurrencyRequest> fullStream(StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
-        return super.fullStream(responseObserver);
+        return new StreamObserver<Bank.ConvertCurrencyRequest>() {
+            @Override
+            public void onNext(Bank.ConvertCurrencyRequest convertCurrencyRequest) {
+                Bank.ConvertCurrencyResponse response = Bank.ConvertCurrencyResponse
+                        .newBuilder()
+                        .setResult(convertCurrencyRequest.getAmount() * 54)
+                        .build();
+
+                responseObserver.onNext(response);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
     }
 
 
